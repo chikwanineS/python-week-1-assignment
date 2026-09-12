406bab0bcf7db9bae097d781bfd7869d4d3cb874
chikwanineS <samuelchikwanine69@gmail.com>
2026-09-12 05:38:55 -0700

    Initial commit for all about me file

---
+++ AllAboutMe.py
@@ -0,0 +1,11 @@
+ 
+
+name = "Samuel"
+age = 25
+favourite_language = "English"
+goal = "to become a skilled software developer"
+
+print("My name is", name + ".")
+print("I am", age, "years old.")
+print("My favourite programming language is", favourite_language + ".")
+print("My goal for this course is", goal + ".")
